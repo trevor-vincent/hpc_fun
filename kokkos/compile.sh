360a6b434dcf88ad95e5341ce372ccd070a8865d
Trevor Vincent <trevor@xanadu.ai>
2022-01-24 19:34:24 -0500

    add new stuff and things

---
+++ kokkos/compile.sh
@@ -0,0 +1,1 @@
+g++ ex_0.cpp -o ex_0 -fopenmp -I../../kokkos/core/src -L../../kokkos/build/core/src -lkokkoscore -I../../kokkos/build -ldl
---
+++ kokkos/run.sh
@@ -0,0 +1,1 @@
+export OMP_PROC_BIND=spread && export OMP_PLACES=threads && ./ex_0
